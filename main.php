e9146b526e552aec555191a82097420a2ec1ec13
3starblaze <3starblaze@protonmail.com>
2022-04-23 09:23:37 +0300

    Add Dotenv, reconfigure docker images
    Reconfiguration includes:
    - linking the whole project to apache so that it can import php files,
    while only exposing public/ folder to the Internet
    - add mysqli pdo adapter to be able to connect to the database

--- main.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$pdo = new PDO("mysql:dbname=testdb;host=127.0.0.1", "user", "password");
-echo("Great success!");
--- public/index.php
+++ public/index.php
@@ -4,6 +4,21 @@
     </head>
     <body>
         <h1>Hello, World!</h1>
+        <?php
+        require __DIR__ . '/../vendor/autoload.php';
+
+        use Dotenv\Dotenv;
+
+        Dotenv::createImmutable(__DIR__.'/../')->load();
+
+        $pdo = new PDO(
+            sprintf("mysql:dbname=%s;host=%s", $_ENV['DB_NAME'], $_ENV['DB_HOST']),
+            $_ENV['DB_USERNAME'],
+            $_ENV['DB_PASSWORD'],
+        );
+
+        echo("Great success!");
+        ?>
         <div id="root"></div>
         <script src="./app.bundle.js"></script>
     </body>
